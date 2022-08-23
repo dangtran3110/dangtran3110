e46a8d143e09a4830c1b793db04cd2bc4360fcae
Trần Hải Đăng <104581286+dangtran3110@users.noreply.github.com>
2022-08-24 03:23:24 +0700

    Reg.php

---
+++ filereg.php
@@ -0,0 +1,112 @@
+<?php
+system("clear");
+echo (" \033[1;92m
+═════════════════════════════════════════════
+Dev;Trần Hải Đăng.                        FB; Hải Đăng 
+HOTLINE; 0901349948.                   YT; Trunks Gaming
+═════════════════════════════════════════════\n");
+echo ("\033[1;96m Day La Regedit Mobile Duoc Lam Boi Tran Hai Dang");
+echo ("\n");
+sleep (1);
+echo "\033[1;93m Password;   ";
+$pass = trim(fgets(STDIN));
+if($pass == "HaiDang"){
+	echo "\033[1;92m Password√\n";
+	sleep(1);
+    echo "\033[1;93m Day La Reg Khong Phai Hack Hay Cheat Nen Ko Ban Acc 100%\n";
+    sleep (1);
+}else{
+echo "\033[1;91m Password×";
+exit;
+}
+echo "\033[1;96m Bạn có chắc đã mua bên Trần Hải Đăng\n";
+sleep (1);
+echo "\033[1;92m Y/N\n";
+sleep (1);
+$pass = trim(fgets(STDIN));
+ if($pass == "Y"){
+echo "Đang Gửi Lệnh Yều Cầu Cấp Quyền Truy Cập File\n";
+sleep (5);
+}else{
+echo "\033[1;91m Không Hoàn Thành";
+exit;
+}
+error_reporting(0);
+session_start();
+system ('clear');
+echo (" \033[1;92m
+═════════════════════════════════════════════
+Dev;Trần Hải Đăng.                        FB; Hải Đăng 
+HOTLINE; 0901349948.                   YT; Trunks Gaming
+═════════════════════════════════════════════\n");
+echo ("\033[1;96m Day La Regedit Mobile Duoc Lam Boi Tran Hai Dang");
+echo ("\n");
+sleep (1);
+while(true){
+$checkkey = file('https://pastebin.com/raw/J0sLbUHv');
+echo "\033[1;96mLưu Ý Mỗi Lượt Cài Xong Key Sẽ Tự Thay Đổi\n";
+echo "\033[1;92mNhập Key Để Cấp Quyền Cài Reg; \033[1;33m";
+      $makey = trim(fgets(STDIN));
+    if ($makey == $checkkey[0])  {
+    sleep(1);
+        echo "\033[1;35mKey Đúng Đang Truy Cập File\n";
+        break;
+    } else {
+        echo "\033[1;91mKey Sai Vui Lòng Lấy Lại\n";
+        echo "\033[1;93m-------------------------------------------------------------\n";
+        sleep(1);
+       continue;
+       }
+}
+echo ("Đã xác nhận cài file Reg Limited 5 User Thành Công\n");
+sleep (5);
+echo "\033[1;96m Bạn xác nhận sử dụng file Reg Limited 5 User\n";
+echo "\033[1;92m Y/N\n";
+$pass = trim(fgets(STDIN));
+ if($pass == "Y"){
+echo "File Đang Bắt Đầu Chạy\n";
+sleep (1);
+}else{
+echo "\033[1;91m Không Hoàn Thành";
+exit;
+}
+echo ("\033[1;95m Reg Đã Được Bật\n");
+sleep (1);
+echo ("\033[1;95m Sensitivity_Setting; High\n");
+sleep (1);
+echo ("\033[1;95m Display_Setting; High\n");
+sleep (5);
+echo ("\033[1;95m DNS_Setting; High\n");
+sleep (5);
+echo ("\033[1;95m CPU_Setting; High\n");
+sleep (5);
+echo ("\033[1;95m Swap_Ram; High\n");
+sleep (5);
+echo ("\033[1;95m Aim_Lock; High\n ");
+sleep (5);
+echo ("\033[1;95m FPS_Setting; High\n ");
+sleep (5);
+echo ("\033[1;95m Tất Cả Chế Độ Đều Được Chỉnh Lên Cao Nhất\n");
+sleep (3);
+echo ("\033[1;91m Hãy Đảm Bảo File Đã Ở Trong Máy Để Chuẩn Bị Dữ Liệu Sẵn Sàng\n");
+sleep(2);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 5\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 4\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 3\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 2\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 1\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 0\n");
+sleep (1);
+echo ("\033[1;95m Reg Đang Tiến Hành Chạy \n");
+sleep (1);
+while(true){
+echo ("\033[1;92m Reg Đang Chạy ");
+echo ("\n");
+sleep (1);
+}
+?>
